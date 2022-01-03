8a8b558910bc72852603fe2ec57e29b021637ee1
jvregalado <jvregalado@codedisruptors.com>
2022-01-03 13:56:30 +0800

    login frontend

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -7,22 +7,48 @@ import Typography from '@mui/material/Typography';
 //import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import {Grid} from '@mui/material'
+import {Grid,Menu,MenuItem} from '@mui/material'
+import {useNavigate} from "react-router-dom"
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  let navigate = useNavigate();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (e) => {
+    const v = e.currentTarget.getAttribute('value');
+    navigate(v);
+    setAnchorEl(null);
+  };
     return (
     <Grid item md={12} sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
+          
+        <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleClick}
           >
             <MenuIcon />
           </IconButton>
+        <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose} value="Create Account">Create Account</MenuItem>
+        <MenuItem onClick={handleClose} value="/">Conversion Tool</MenuItem>
+        <MenuItem onClick={handleClose} value="/login">Logout</MenuItem>
+      </Menu>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Conversion Tool
           </Typography>
